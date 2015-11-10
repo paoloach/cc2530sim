a6cddc645062a6af1d27b1b4f1a22d2e6843408a
Paolo Achdjian <pachdjian@gmail.com>
2015-11-10 16:46:36 +0000

    work in progress

---
+++ loader/HexLine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 11/10/15.
+//
+
+#include "HexLine.h"
---
+++ loader/HexLine.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 11/10/15.
+//
+
+#ifndef CC2530_HEXLINE_H
+#define CC2530_HEXLINE_H
+
+
+class HexLine {
+
+};
+
+
+#endif //CC2530_HEXLINE_H
